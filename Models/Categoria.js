b8c6acf863eda4080857677d23982bf7e701859f
Gerardo <breyneracosta7@gmail.com>
2025-03-28 11:38:36 -0500

    api

---
+++ Models/Categoria.js
@@ -0,0 +1,36 @@
+import connection from "../utils/db.js";
+
+class Categoria {
+  constructor(nombre, descripcion) {
+    this.nombre = nombre;
+    this.descripcion = descripcion;
+  }
+
+  /**
+   * Método para obtener los registros de la base de datos
+   * @returns {Array} Listado de las categorías en un arreglo
+   */
+  async getAll() {
+    try {
+      const [rows] = await connection.query("SELECT * FROM categorias");
+      return rows;
+    } catch (error) {
+      throw new Error("Error al obtener las categorías");
+    }
+  }
+
+  async create() {
+    try {
+      const [result] = await connection.query("INSERT INTO categorias (nombre, descripcion) VALUES (?,?)", [this.nombre, this.descripcion]);
+      return {
+        id: result.id,
+        nombre: this.nombre,
+        descripcion: this.descripcion
+      };
+    } catch (error) {
+      throw new Error("Error al guardar categoría");
+    }
+  }
+};
+
+export default Categoria;
